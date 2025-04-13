8ca4251cb2ebf910380cd66f53481c7a2602eb40
Nodirov Jakhongir <jakhon37@gmail.com>
2025-04-13 10:28:33 +0900

    control hardware code

--- src/create_pkg.bash
+++ src/create_pkg.bash
@@ -1,5 +1,5 @@
 
-ros2 pkg create --build-type ament_python motor_control --dependencies rclpy geometry_msgs
+ros2 pkg create --build-type ament_python control_hardware --dependencies rclpy geometry_msgs
 ros2 pkg create --build-type ament_python mpu6050_imu --dependencies rclpy sensor_msgs
 pip3 install smbus2
 

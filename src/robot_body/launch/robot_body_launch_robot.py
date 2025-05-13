6fbc47ee891d86846fb0916d1a4b94706cade2ce
Nodirov Jakhongir <jakhon37@gmail.com>
2025-05-13 10:58:11 +0900

    controller

--- src/robot_body/launch/robot_body_launch_robot.py
+++ src/robot_body/launch/robot_body_launch_robot.py
@@ -1,69 +1,65 @@
-
-# autoJetsonBot/src/robot_body/launch/robot_body_launch_robot.py
-
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch_ros.actions import Node
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
 
-
 def generate_launch_description():
+    package_name = 'robot_body'
 
+    # Log URDF path for debugging
+    urdf_path = os.path.join(get_package_share_directory(package_name), 'urdf', 'robot.xacro')
+    log_urdf_path = LogInfo(msg=f"URDF path: {urdf_path}")
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='robot_body'
-
+    # Include the robot_state_publisher launch file
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','robot_body_launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'robot_body_launch.py'
+        )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    
-    # gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    # gazebo = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    #             launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-    #          )
+    # Robot state publisher
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{"robot_description": Command(['xacro ', urdf_path, ' use_ros2_control:=true sim_mode:=false'])}]
+    )
 
-    # # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    # spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-    #                     arguments=['-topic', 'robot_description',
-    #                                '-entity', 'my_bot'],
-    #                     output='screen')
-
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    # Controller manager
     controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
-
-
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{"robot_description": robot_description},
-                    controller_params_file
-                    ],
+        parameters=[
+            {"robot_description": Command(['xacro ', urdf_path, ' use_ros2_control:=true sim_mode:=false'])},
+            controller_params_file
+        ],
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+        ],
     )
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    # Delay controller manager until robot_state_publisher starts
+    controller_manager_event_handler = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=robot_state_publisher,
+            on_start=[controller_manager]
+        )
+    )
 
+    # Spawner for diff_drive_controller
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner.py",
         arguments=["diff_cont"],
     )
-    
+
     delayed_diff_drive_spawner = RegisterEventHandler(
         event_handler=OnProcessStart(
             target_action=controller_manager,
@@ -71,53 +67,41 @@ def generate_launch_description():
         )
     )
 
-
-    delayed_joint_broad_spawner = RegisterEventHandler(
-    event_handler=OnProcessStart(
-        target_action=controller_manager,
-        on_start=[joint_broad_spawner]
-    )
-    )
-
-    
+    # Spawner for joint_state_broadcaster
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner.py",
         arguments=["joint_broad"],
     )
-    
+
     delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handlers=OnProcessStart(
+        event_handler=OnProcessStart(
             target_action=controller_manager,
             on_start=[joint_broad_spawner]
         )
     )
 
-
-
+    # Serial bridge node
     bridge_node = Node(
-    package='robot_body',
-    executable='serial_bridge',
-    name='serial_bridge',
-    parameters=[{
-        'serial_port': '/dev/ttyUSB0',
-        'baudrate': 115200,
-        'wheel_separation': 0.297,
-        'wheel_radius': 0.033,
-        'max_wheel_linear': 0.5
-    }]
+        package='robot_body',
+        executable='serial_bridge',
+        name='serial_bridge',
+        parameters=[{
+            'serial_port': '/dev/ttyACM0',
+            'baudrate': 115200,
+            'wheel_separation': 0.18,  # Match my_controllers.yaml
+            'wheel_radius': 0.035,    # Match my_controllers.yaml
+            'max_wheel_linear': 0.5
+        }]
     )
 
-
-    # Launch them all!
+    # Launch everything
     return LaunchDescription([
+        log_urdf_path,
         rsp,
-        # gazebo,
-        # spawn_entity,
-        delayed_controller_manager,
+        robot_state_publisher,
+        controller_manager_event_handler,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner,
-        # diff_drive_spawner,
-        # joint_broad_spawner,
         bridge_node
     ])

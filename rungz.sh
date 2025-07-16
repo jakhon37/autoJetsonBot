fe5f54ac5558404f552918c1bb1f57e3710e7edf
NODIROV JAKHONGIR <jakhon37@gmail.com>
2025-07-17 00:10:21 +0900

    new int

--- rungz.sh
+++ rungz.sh
@@ -10,8 +10,8 @@ source install/setup.bash
 
 # ros2 launch my_robot_launch robot_body_launch_sim.py  
 # ros2 launch my_robot_launch robot_body_launch_sim.py  world:=./src/my_robot_launch/config/cafe.world
-ros2 launch my_robot_launch robot_body_launch_sim.py  sim_mode:=true  world:=./src/my_robot_launch/config/lab.world
-
+ros2 launch my_robot_launch robot_body_launch_sim.py  use_sim_time:=true   world:=./src/my_robot_launch/config/lab.world
+# sim_mode:=true
 # ros2 launch slam_toolbox online_async_launch.py params_file:=./src/slam_launch/config/mapper_params_online_async.yaml 
 # ros2 run rviz2 rviz2 -d ~/myspace/autoJetsonBot/src/my_robot_launch/config/main.rviz --ros-args --param use_sim_time:=true
 
@@ -27,3 +27,6 @@ ros2 launch my_robot_launch robot_body_launch_sim.py  sim_mode:=true  world:=./s
 #     x:=5.0 y:=5.0 z:=0.5 \
 #     bridge_name:=ros_gz_bridge \
 #     config_file:=gz_conf.xml
+
+
+
